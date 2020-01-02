8da9fabce8d42479a1975dc432ace5e350a766e2
Nathan Ramaker <nathan.ramaker@helpsystems.com>
2020-01-02 15:30:49 -0600

    BurgerIngredient finished

--- burgers/src/components/Layout/Layout.js
+++ burgers/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Wrapper from '../../hoc/Wrapper'
+import Wrapper from '../../hoc/Wrapper';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Wrapper>
         <div>Toolbar, Sidedraw, Backdrop</div>
-        <main>
+        < main className={classes.Content}>
             {props.children}
         </main>
     </Wrapper>
