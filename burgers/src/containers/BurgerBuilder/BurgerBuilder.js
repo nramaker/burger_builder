e190c4c09e9c4cd64b2cfb4f1cb0f7403c805faa
Nathan Ramaker <nathan.ramaker@helpsystems.com>
2020-01-07 13:47:34 -0600

    Buttons

--- burgers/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgers/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -80,6 +80,14 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You continued!');
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -89,8 +97,11 @@ class BurgerBuilder extends Component {
         }
         return (
             <Wrapper>
-                <Modal show={this.state.purchasing}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCanceled = {this.purchaseCancelHandler}
+                        purchaseContinued = {this.purchaseContinueHandler}/>
                 </Modal>
                 <Burger ingredients = {this.state.ingredients}/>
                 <BuildControls 
